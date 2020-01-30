82db3a06a1818e376557304fc45e3e437bc1c19f
Jongkook <conquerex@gmail.com>
2020-01-30 17:24:47 +0900

    학습 : 10799

--- src/Algorithm17298.java
+++ src/Algorithm17298.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
  * 문제 : url
  * 풀이 : url
  */
-public class AlgorithmTemplate {
+public class Algorithm17298 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
---
+++ src/AlgorithmTemp.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * (문제 이름)
+ * 문제 : url
+ * 풀이 : url
+ */
+public class AlgorithmTemp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder builder = new StringBuilder();
+        System.out.println(builder);
+    }
+}
