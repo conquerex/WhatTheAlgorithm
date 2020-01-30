ef040a0c59c85b2aafcc5cc310b9fc625ef0a823
Jongkook <conquerex@gmail.com>
2020-01-30 16:45:36 +0900

    학습 : 17413

---
+++ src/Algorithm10799.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+/**
+ * 쇠막대기
+ * 문제 : https://www.acmicpc.net/problem/10799
+ * 풀이 :
+ */
+public class Algorithm10799 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder builder = new StringBuilder();
+        System.out.println(builder);
+    }
+}
