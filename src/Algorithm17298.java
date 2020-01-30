44c0e40c7967f21aafc22276e4209b0f60900f3a
Jongkook <conquerex@gmail.com>
2020-01-30 18:05:05 +0900

    학습중 : 17298

--- src/Algorithm17298.java
+++ src/Algorithm17298.java
@@ -1,4 +1,7 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 /**
  * (문제 이름)
@@ -6,10 +9,32 @@ import java.util.Scanner;
  * 풀이 : url
  */
 public class Algorithm17298 {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        StringBuilder builder = new StringBuilder();
-        System.out.println(builder);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int size = Integer.parseInt(reader.readLine());
+        int[] list = new int[size];
+        int[] answer = new int[size];
+        String[] temp = reader.readLine().split(" ");
+        for (int i = 0; i < size; i++) {
+            list[i] = Integer.parseInt(temp[i]);
+        }
+
+        Stack<Integer> position = new Stack<>();
+        position.push(0);
+        for (int i = 1; i < size; i++) {
+            if (position.isEmpty()) {
+                position.push(i);
+            }
+
+            while (!position.isEmpty() && list[position.peek()] < list[i]) {
+                answer[position.pop()] = list[i];
+            }
+
+            position.push(i);
+        }
+
+        while (!position.empty()) {
+
+        }
     }
 }
