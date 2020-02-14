8d500d8e272af33284f4584a6aec380557228641
Jongkook <conquerex@gmail.com>
2020-02-14 15:18:26 +0900

    Updates 저장

---
+++ src/AlgorithmTest.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+/**
+ * 테스트(과제)용
+ */
+public class AlgorithmTest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder builder = new StringBuilder();
+        System.out.println(builder);
+    }
+}
