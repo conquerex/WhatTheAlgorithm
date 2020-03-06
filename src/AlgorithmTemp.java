f1bb1c187fb9c0f2f45313982304ff75eecc14e2
Jongkook <conquerex@gmail.com>
2020-03-06 15:09:26 +0900

    학습 : 2193

--- src/AlgorithmTemp.java
+++ src/AlgorithmTemp.java
@@ -9,7 +9,7 @@ public class AlgorithmTemp {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        StringBuilder builder = new StringBuilder();
-        System.out.println(builder);
+
+        System.out.println(n);
     }
 }
