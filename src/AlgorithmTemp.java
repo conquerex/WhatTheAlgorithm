a0f5905deffd116a07566bd788cbd927d1183549
Jongkook <conquerex@gmail.com>
2020-03-09 18:43:21 +0900

    학습 : 13398

--- src/AlgorithmTemp.java
+++ src/AlgorithmTemp.java
@@ -4,9 +4,6 @@ import java.util.Scanner;
  * (문제 이름, class 원래 이름 - Main)
  * 문제 : url
  * 풀이 : url
- *
- * [진행할 문제들]
- * 연속합2
  */
 public class AlgorithmTemp {
     public static void main(String[] args) {
