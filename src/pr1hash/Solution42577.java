83878c48d8ef17dae0bce2cd23caa218b38547da
Jongkook <conquerex@gmail.com>
2020-03-30 16:03:41 +0900

    프로그래머스 : 해시 2

--- src/pr1hash/Solution42577.java
+++ src/pr1hash/Solution42577.java
@@ -1,30 +1,31 @@
 package pr1hash;
 
-import java.util.Arrays;
-
 /**
- * 완주하지 못한 선수
- * 문제 : https://programmers.co.kr/learn/courses/30/lessons/42576
- * 풀이 : https://programmers.co.kr/learn/courses/30/lessons/42576/solution_groups?language=java
+ * 전화번호부에 적힌 전화번호를 담은 배열 phone_book 이 solution 함수의 매개변수로 주어질 때,
+ * 어떤 번호가 다른 번호의 접두어인 경우가 있으면 false를
+ * 그렇지 않으면 true를 return 하도록 solution 함수를 작성해주세요.
+ * [119, 97674223, 1195524421]
  */
 public class Solution42577 {
     public static void main(String[] args) {
 
-        String[] part = {"marina", "josipa", "nikola", "vinko", "filipa", "test"};
-        String[] comp = {"josipa", "filipa", "marina", "vinko", "nikola"};
+        String[] book = {"119", "97674223", "1195524421"};
 
-        System.out.println(solution(part, comp));
+        System.out.println(solution(book));
     }
 
-    public static String solution(String[] participant, String[] completion) {
-        Arrays.sort(participant);
-        Arrays.sort(completion);
-        int i;
-        for (i = 0; i < completion.length; i++) {
-            if (completion[i] != participant[i]) {
-                return participant[i];
+    public static boolean solution(String[] phone_book) {
+        boolean answer = true;
+        for (int i = 0 ; i < phone_book.length ; i++) {
+            for (int j = 0 ; j < phone_book.length ; j++) {
+                if (phone_book[i] == phone_book[j]) continue;
+                if (phone_book[i].length() >= phone_book[j].length()) continue;
+                if (phone_book[i].equals(phone_book[j].substring(0,phone_book[i].length()))) {
+                    return false;
+                }
             }
         }
-        return participant[i];
+
+        return answer;
     }
 }
