944bcf07dbbc0983e2157b07ce28763c81bbc94a
Jongkook <conquerex@gmail.com>
2020-04-07 18:23:57 +0900

    학습 : 14889

---
+++ src/part2bitmask/Algorithm14391.java
@@ -0,0 +1,17 @@
+package part2bitmask;
+
+import java.util.Scanner;
+
+/**
+ * (문제 이름, class 원래 이름 - Main)
+ * 문제 : url
+ * 풀이 : url
+ */
+public class Algorithm14391 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(n);
+    }
+}
