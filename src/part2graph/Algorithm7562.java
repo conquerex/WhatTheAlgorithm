d74f55f75c690016a441047a5137d9a705008944
Jongkook <conquerex@gmail.com>
2020-04-10 18:31:44 +0900

    학습 : 2667

---
+++ src/part2graph/Algorithm7562.java
@@ -0,0 +1,22 @@
+package part2graph;
+
+import java.util.Scanner;
+
+/**
+ * (문제 이름, class 원래 이름 - Main)
+ * 문제 : url
+ * 풀이 : url
+ */
+public class Algorithm7562 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        for (int i = 0; i < m; i++) {
+            //
+        }
+
+        System.out.println(n);
+    }
+}
