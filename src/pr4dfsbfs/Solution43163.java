a65e20b4b6fc4ba8c1c712d9c6c785ac79e1648c
Jongkook <conquerex@gmail.com>
2020-05-05 23:22:04 +0900

    프로그래머스 : DFS,BFS

---
+++ src/pr4dfsbfs/Solution43163.java
@@ -0,0 +1,71 @@
+package pr4dfsbfs;
+
+public class Solution43163 {
+
+    static boolean[] check;
+    static String text;
+    static int result = 0;
+
+    static public int solution(String begin, String target, String[] words) {
+        boolean pass = false;
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(target)) {
+                pass = true;
+            }
+        }
+
+        if (!pass) {
+            return 0;
+        }
+
+        int answer = 0;
+        check = new boolean[words.length];
+        text = begin;
+
+        while (!text.equals(target)) {
+            notBfs(target, words);
+        }
+
+        answer = result;
+        return answer;
+    }
+
+    static public boolean compareWords(String word, String target) {
+        int changeCount = 0;
+        for (int i = 0; i < target.length(); i++) {
+            if (word.charAt(i) != target.charAt(i)) {
+                changeCount += 1;
+            }
+        }
+
+        if (changeCount == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    static public void notBfs(String target, String[] words) {
+        if (compareWords(text, target)) {
+            result += 1;
+            text = target;
+            return;
+        }
+
+        for (int m = 0; m < words.length; m++) {
+            if (check[m] == false && compareWords(text, words[m])) {
+                text = words[m];
+                check[m] = true;
+                result += 1;
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String words[] = {"hot", "dot", "dog", "lot", "log", "cog"};
+        String begin = "hit";
+        String target = "cog";
+        System.out.println(solution(begin, target, words));
+    }
+}
---
+++ src/pr4dfsbfs/Solution43163_2.java
@@ -0,0 +1,79 @@
+package pr4dfsbfs;
+
+import java.util.*;
+
+public class Solution43163_2 {
+
+    static List<Stack<String>> result;
+    static String[][] ticketArray;
+
+    public static String[] solution(String[][] tickets) {
+        result = new ArrayList<>();
+        ticketArray = tickets;
+
+        boolean[] visited = new boolean[tickets.length];
+        Stack<String> stack = new Stack<>();
+        stack.push("ICN");
+
+        showDfs(visited, stack, 0);
+
+        if (result.size() > 1) {
+            Collections.sort(result, new Comparator<Stack<String>>() {
+                @Override
+                public int compare(Stack<String> t0, Stack<String> t1) {
+                    for (int i = 0; i < t0.size(); i++) {
+                        String s0 = t0.get(i);
+                        String s1 = t1.get(i);
+
+                        if (!s0.equals(s1)) {
+                            return s0.compareTo(s1);
+                        }
+                    }
+                    return 0;
+                }
+            });
+        }
+
+        Stack<String> res = result.remove(0);
+        String[] answer = new String[res.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = res.get(i);
+        }
+
+        return answer;
+    }
+
+    public static void showDfs(boolean[] visited, Stack<String> stack, int len) {
+        if (len == ticketArray.length) {
+            Stack<String> res = new Stack<>();
+            for (String s : stack) {
+                res.push(s);
+            }
+
+            result.add(res);
+            return;
+        }
+
+        String arrive = stack.peek();
+
+        for (int i = 0; i < ticketArray.length; i++) {
+            String[] tic = ticketArray[i];
+            if (!visited[i] && arrive.equals(tic[0])) {
+                stack.push(tic[1]);
+                visited[i] = true;
+
+                showDfs(visited, stack, len + 1);
+                visited[i] = false;
+                stack.pop();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[][] t = {{"ICN", "JFK"}, {"HND", "IAD"}, {"JFK", "HND"}};
+        String[] result = solution(t);
+        for (String s : result) {
+            System.out.println(s);
+        }
+    }
+}
