ca88385a3a4ab1ebf884f8c067b0665c955320d8
Jongkook <conquerex@gmail.com>
2020-05-21 05:05:26 +0900

    week1-2

---
+++ src/study/week1/week1_00004.java
@@ -0,0 +1,23 @@
+package study.week1;
+
+/**
+ * https://leetcode.com/problems/climbing-stairs/
+ */
+public class week1_00004 {
+
+    public static void main(String[] args) {
+        System.out.println(climbStairs(5));
+    }
+
+    public static int climbStairs(int n) {
+        if(n == 0 || n == 1 || n == 2){return n;}
+        int[] mem = new int[n];
+        mem[0] = 1;
+        mem[1] = 2;
+        for(int i = 2; i < n; i++){
+            mem[i] = mem[i-1] + mem[i-2];
+        }
+        return mem[n-1];
+    }
+
+}
