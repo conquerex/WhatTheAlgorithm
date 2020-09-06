5717aacb7b2c84d47e878a9332e98173032b3370
Jongkook <conquerex@gmail.com>
2020-09-07 07:43:18 +0900

    week16_01966

--- src/study/week16/week16_01966.java
+++ src/study/week16/week16_01966.java
@@ -8,11 +8,6 @@ import java.util.*;
 /**
  * Created by Jongkook on 2020/09/07.
  * 문제 출처 : https://www.acmicpc.net/problem/1966
- * 첫 줄에 test case의 수가 주어진다. 각 test case에 대해서
- * 문서의 수 N(100이하)와
- * 몇 번째로 인쇄되었는지 궁금한 문서가 현재 Queue의 어떤 위치에 있는지를 알려주는 M(0이상 N미만)이 주어진다.
- * 다음줄에 N개 문서의 중요도가 주어지는데, 중요도는 1 이상 9 이하이다.
- * 중요도가 같은 문서가 여러 개 있을 수도 있다. 위의 예는 N=4, M=0(A문서가 궁금하다면), 중요도는 2 1 4 3이 된다.
  * <p>
  * Time Complexity :
  * Used Algorithm : 시뮬레이션, 큐
@@ -33,14 +28,16 @@ public class week16_01966 {
                 Queue<Integer> queue = new LinkedList<>();
                 // Collections.reverseOrder : 높은 수부터 출력
                 Queue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
-                for (int j = 0; j < docs.countTokens(); j++) {
+
+                // docs의 count는 nextToken마다 변경되기 때문에 n 자리에 docs.countTokens를 사용하면 안됨
+                for (int j = 0; j < n; j++) {
                     int doc = Integer.parseInt(docs.nextToken());
                     queue.offer(doc);
-                    priorityQueue.offer(doc);
                 }
+                priorityQueue.addAll(queue);
 
                 while (!queue.isEmpty()) {
-                    if (queue.peek() == priorityQueue.peek()) {
+                    if (queue.peek().equals(priorityQueue.peek())) {
                         // queue에서 뽑은 것이 가장 큰 것인지 확인
                         result++;
                         if (m == 0) {
