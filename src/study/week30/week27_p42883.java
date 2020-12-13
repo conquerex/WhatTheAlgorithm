b9e2483b896a3c32e9e8abd2c0ddf432712ee7b3
Jongkook <conquerex@gmail.com>
2020-12-13 16:04:34 +0900

    week27_p42883

--- src/study/week30/week27_p42883.java
+++ src/study/week30/week27_p42883.java
@@ -15,20 +15,29 @@ public class week27_p42883 {
     }
 
     public static String solution(String number, int k) {
-        String answer = "";
-        int removePoint = k;
-        // 일단 k번째 숫자 중 가장 큰것을 구하자
-        char[] nums = number.toCharArray();
-        char[] result = new char[nums.length - k];
-        for (int i = 0; i < k; i++) {
-            if (result.length > 0) {
-                for (int j = 0; j < result.length; j++) {
+        int length = number.length();
+        // 반드시 남아야하는 숫자 갯수
+        int count = length - k;
 
+        StringBuilder sb = new StringBuilder(count);
+
+        int index = -1;
+        while (count > 0) {
+            int max = 0;
+            // 지정된 인덱스 다음 ~ 반드시 남아 있어야하는 숫자열
+            for (int i = index + 1; i <= length - count; i++) {
+                int num = number.charAt(i) - '0';
+                if (num > max) {
+                    max = num;
+                    index = i;
                 }
-            } else {
-                result[0] = nums[0];;
             }
+            // 가장 큰 값을 하나씩 추가
+            // (범위 : 지정된 인덱스 다음 ~ 반드시 남아 있어야하는 숫자열)
+            sb.append(max);
+            count--;
         }
-        return answer;
+
+        return sb.toString();
     }
 }
