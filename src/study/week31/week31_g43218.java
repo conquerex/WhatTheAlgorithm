e5d9fb8d9596441427284d801ac068b7a76bf974
Jongkook <conquerex@gmail.com>
2020-12-20 14:22:23 +0900

    week31_g43218

--- src/study/week31/week31_g43218.java
+++ src/study/week31/week31_g43218.java
@@ -2,7 +2,6 @@ package study.week31;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 /**
  * Created by Jongkook on 2020/12/20.
@@ -15,73 +14,75 @@ import java.util.Arrays;
 
 public class week31_g43218 {
     public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Boolean isStatus = false;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            Boolean isStatus = false;
 
-        int count = 0;
-        String temp = "";
-        Boolean isPop = false;
-        int ppCount = 0; // push & pop count 값
-        String[] stdInArr = new String[count];
-        int totalCount = Integer.parseInt(br.readLine());
+            int count = 0;
+            String temp = "";
+            Boolean isPop = false;
+            int ppCount = 0; // push & pop count 값
+            String[] stdInArr = new String[count];
+            int totalCount = Integer.parseInt(br.readLine());
 
-        while (isStatus) {
-            String input = br.readLine();
-            if (count > 10) {
-                System.out.print("overflow");
-                break;
-            }
+            while (isStatus) {
+                String input = br.readLine();
+                if (count > 10) {
+                    System.out.print("overflow");
+                    break;
+                }
 
-            if (count % 2 == 0) {
-                // >>>>> push & pop 입력 <<<<<
-                temp = input;
+                if (count % 2 == 0) {
+                    // >>>>> push & pop 입력 <<<<<
+                    temp = input;
 
-                if (input.equals("1")) {
-                    // case : POP
-                    isPop = true;
+                    if (input.equals("1")) {
+                        // case : POP
+                        isPop = true;
 
-                    if (ppCount == 0) {
-                        //무조건 첫번째면 underflow
-                        System.out.println("underflow");
-                    } else {
-                        ppCount--;
-                        if (stdInArr[ppCount] == null) {
+                        if (ppCount == 0) {
+                            //무조건 첫번째면 underflow
                             System.out.println("underflow");
                         } else {
-                            stdInArr[ppCount] = null;
+                            ppCount--;
+                            if (stdInArr[ppCount] == null) {
+                                System.out.println("underflow");
+                            } else {
+                                stdInArr[ppCount] = null;
+                            }
                         }
+                        break; // 푸시인 경우에만 자료의 내용을 다음 줄에 입력합니다.
                     }
-                    break; // 푸시인 경우에만 자료의 내용을 다음 줄에 입력합니다.
-                }
-            } else {
-                // >>>>> push & pop 실행 <<<<<
-                if (temp.equals("0")) { // push
-                    stdInArr[ppCount] = input;
-                    ppCount++;
-                } else if (temp.equals("1")) { // pop
-                    //
                 } else {
-                    return; //0 or 1이 아닌 다른값이면 시스템 종료
+                    // >>>>> push & pop 실행 <<<<<
+                    if (temp.equals("0")) { // push
+                        stdInArr[ppCount] = input;
+                        ppCount++;
+                    } else if (temp.equals("1")) { // pop
+                        //
+                    } else {
+                        return; //0 or 1이 아닌 다른값이면 시스템 종료
+                    }
+
+                    temp = ""; //초기화
                 }
 
-                temp = ""; //초기화
+                count++;
+
+                if (count == totalCount * 2) {
+                    isStatus = true;
+                }
             }
 
-            count++;
-
-            if (count == totalCount * 2) {
-                isStatus = true;
+            String str = "";
+            if (!isPop) {
+                //
+            } else {
+                //
             }
+
+            System.out.print(str);
+        } catch (Exception e) {
+            e.printStackTrace();
         }
-
-        String str = "";
-        if (!isPop) {
-            //
-        } else {
-            //
-        }
-
-        System.out.print(str);
-
     }
 }
