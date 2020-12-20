c135584a24ab9bae987352773e84d90dfb3e8184
Jongkook <conquerex@gmail.com>
2020-12-20 14:40:45 +0900

    week31_g43218

--- src/study/week31/week31_g43218.java
+++ src/study/week31/week31_g43218.java
@@ -2,6 +2,7 @@ package study.week31;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 /**
  * Created by Jongkook on 2020/12/20.
@@ -14,73 +15,36 @@ import java.io.InputStreamReader;
 
 public class week31_g43218 {
     public static void main(String[] args) throws Exception {
+        final String OVERFLOW = "overflow";
+        final String UNDERFLOW = "underflow";
+        final int MAX_SIZE = 10;
+
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
-            Boolean isStatus = false;
+            ArrayList<Integer> stack = new ArrayList<>();
 
-            int count = 0;
-            String temp = "";
-            Boolean isPop = false;
-            int ppCount = 0; // push & pop count 값
-            String[] stdInArr = new String[count];
-            int totalCount = Integer.parseInt(br.readLine());
+            // 입력횟수
+            int maxCount = Integer.parseInt(br.readLine());
 
-            while (!isStatus) {
-                String input = br.readLine();
-                if (count > 10) {
-                    System.out.print("overflow");
+            for (int i = 0; i < maxCount; i++) {
+                int operation = Integer.parseInt(br.readLine());
+                if (operation == 0) {
+                    // PUSH
+                    int added = Integer.parseInt(br.readLine());
+                    if (stack.size() >= MAX_SIZE) System.out.println(OVERFLOW);
+                    else stack.add(added);
+                } else if (operation == 1) {
+                    // POP
+                    if (stack.isEmpty()) System.out.println(UNDERFLOW);
+                    else stack.remove(stack.size() - 1);
+                } else {
                     break;
                 }
-
-                if (count % 2 == 0) {
-                    // >>>>> push & pop 입력 <<<<<
-                    temp = input;
-
-                    if (input.equals("1")) {
-                        // case : POP
-                        isPop = true;
-
-                        if (ppCount == 0) {
-                            //무조건 첫번째면 underflow
-                            System.out.println("underflow");
-                        } else {
-                            ppCount--;
-                            if (stdInArr[ppCount] == null) {
-                                System.out.println("underflow");
-                            } else {
-                                stdInArr[ppCount] = null;
-                            }
-                        }
-                        break; // 푸시인 경우에만 자료의 내용을 다음 줄에 입력합니다.
-                    }
-                } else {
-                    // >>>>> push & pop 실행 <<<<<
-                    if (temp.equals("0")) { // push
-                        stdInArr[ppCount] = input;
-                        ppCount++;
-                    } else if (temp.equals("1")) { // pop
-                        //
-                    } else {
-                        return; //0 or 1이 아닌 다른값이면 시스템 종료
-                    }
-
-                    temp = ""; //초기화
-                }
-
-                count++;
-
-                if (count == totalCount * 2) {
-                    isStatus = true;
-                }
             }
 
-            String str = "";
-            if (!isPop) {
-                //
-            } else {
-                //
+            for (int i : stack) {
+                // 결과 출력
+                System.out.print(i + " ");
             }
-
-            System.out.print(str);
         } catch (Exception e) {
             e.printStackTrace();
         }
