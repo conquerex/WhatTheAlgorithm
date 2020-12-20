f7227f33ef5846ebe993e3fe92119ce267abd65c
Jongkook <conquerex@gmail.com>
2020-12-20 14:15:35 +0900

    week31_g43218

--- src/study/week31/week31_g43218.java
+++ src/study/week31/week31_g43218.java
@@ -21,7 +21,7 @@ public class week31_g43218 {
         int count = 0;
         String temp = "";
         Boolean isPop = false;
-        int ppCount = 0; //push&pop count 값
+        int ppCount = 0; // push & pop count 값
         String[] stdInArr = new String[count];
         int totalCount = Integer.parseInt(br.readLine());
 
@@ -32,7 +32,8 @@ public class week31_g43218 {
                 break;
             }
 
-            if (count % 2 == 0) { // push & pop 입력
+            if (count % 2 == 0) {
+                // >>>>> push & pop 입력 <<<<<
                 temp = input;
 
                 if (input.equals("1")) {
@@ -50,20 +51,17 @@ public class week31_g43218 {
                             stdInArr[ppCount] = null;
                         }
                     }
-
-                    break; //푸시인 경우에만 자료의 내용을 다음 줄에 입력합니다.
+                    break; // 푸시인 경우에만 자료의 내용을 다음 줄에 입력합니다.
                 }
             } else {
-                // push & pop 실행
+                // >>>>> push & pop 실행 <<<<<
                 if (temp.equals("0")) { // push
-
                     stdInArr[ppCount] = input;
                     ppCount++;
                 } else if (temp.equals("1")) { // pop
-
-
+                    //
                 } else {
-                    return; //0/1 이아닌 다른값이면 시스템 종료
+                    return; //0 or 1이 아닌 다른값이면 시스템 종료
                 }
 
                 temp = ""; //초기화
@@ -77,10 +75,10 @@ public class week31_g43218 {
         }
 
         String str = "";
-        if (isPop == false) {
-            str = Arrays.toString(stdInArr).replace(", ", " ").replace("[", "").replace("]", "");
+        if (!isPop) {
+            //
         } else {
-            str = Arrays.toString(stdInArr).replace(", ", " ").replace("[", "").replace("]", " ").replace("null ", "");
+            //
         }
 
         System.out.print(str);
