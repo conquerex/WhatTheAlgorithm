c2579c9c0d9893eff67a342ab9bcf7d64312928c
Jongkook <conquerex@gmail.com>
2021-01-14 07:01:14 +0900

    week01_03273

--- src/study2021/week01/week01_03273.java
+++ src/study2021/week01/week01_03273.java
@@ -3,7 +3,7 @@ package study2021.week01;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 /**
@@ -18,7 +18,6 @@ import java.util.StringTokenizer;
 public class week01_03273 {
     public static void main(String[] args) {
         long sum = 0;
-        int start = 0, end = 1;
         long result = 0;
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
@@ -27,38 +26,29 @@ public class week01_03273 {
             int x = Integer.parseInt(br.readLine()); // 셋째줄
 
             int[] input = new int[n];
-            PriorityQueue<Integer> queue = new PriorityQueue();
+
+            int start = 0;
+            int end = n - 1; // 중요 : 2개의 포인터를 어디에 맞출 것인가에 따라 속도가 다를 수 있다.
+
             while (st.hasMoreTokens()) {
-//                input[start++] = Integer.parseInt(st.nextToken());
-                queue.add(Integer.parseInt(st.nextToken()));
+                input[start++] = Integer.parseInt(st.nextToken());
             }
 
-            // 다시 초기화
+            // 반드시 start 초기화
             start = 0;
 
             // 투포인터 사용시 반드시 정렬 먼저
-//            List<Integer> list = Arrays.stream(input).sorted().boxed().collect(Collectors.toList());
+            Arrays.parallelSort(input);
 
-            int[] array = new int[queue.size()];
-            for (int i = 0; i < n; i++) {
-                array[i] = queue.poll();
-            }
-
-            while (start < n) {
+            while (start < end) {
                 // sum을 다시 하는 시점이 중요
-                if (end < n)
-//                    sum = list.get(start) + list.get(end);
-                    sum = array[start] + array[end];
-                else
-//                    sum = list.get(start) + list.get(n - 1);
-                    sum = array[start] + array[n - 1];
+                sum = input[start] + input[end];
 
-                if (sum >= x || end == n) {
+                if (sum > x) {
                     // start 위치를 옮기는 동시에 end도 재조정
-                    start++;
-                    end = start + 1;
+                    end--;
                 } else {
-                    end++;
+                    start++;
                 }
 
                 if (sum == x) result++;
